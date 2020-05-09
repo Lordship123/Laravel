ee347a6d1aca26f1449f01c4886bf94d964eb3e8
Lordship123 <egor87768@gmail.com>
2020-05-09 14:54:05 +0300

    create Todoo controller

---
+++ app/Http/Controllers/Todoo/TodooController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Todoo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class new extends Controller
+{
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getTaskToday()
+    {
+        //
+    }
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createTask()
+    {
+        //
+    }
+
+}
